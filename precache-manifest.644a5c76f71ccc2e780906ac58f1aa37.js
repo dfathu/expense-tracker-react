68118308e7437fddbace6bb6d83eb5e3da32ea78
Fathu Danur <dfathu@gmail.com>
2020-07-07 06:46:25 +0700

    Updates

---
+++ precache-manifest.644a5c76f71ccc2e780906ac58f1aa37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d164050fd508033be3fe65865681111",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "c80872a4b5c4b02b63d9",
+    "url": "/expense-tracker-react/static/css/main.da56effb.chunk.css"
+  },
+  {
+    "revision": "743cb3f389247e8c735d",
+    "url": "/expense-tracker-react/static/js/2.b4006025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.b4006025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80872a4b5c4b02b63d9",
+    "url": "/expense-tracker-react/static/js/main.e4a7d0e5.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
